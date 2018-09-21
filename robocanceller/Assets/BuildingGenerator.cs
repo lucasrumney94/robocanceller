b3cfc88687245cf8342c1193367f311c6c4c257d
Lucas Rumney <lucasrumney94@gmail.com>
2018-09-21 10:32:46 -0700

    added ring city generator

--- robocanceller/Assets/BuildingGenerator.cs
+++ robocanceller/Assets/BuildingGenerator.cs
@@ -4,37 +4,42 @@ using UnityEngine;
 
 public class BuildingGenerator : MonoBehaviour {
 
+
 	public GameObject buildingPrefab;
-	public int numberOfBuildings = 10;
-	public float radius = 5.0f;
-	public float heightVariance = 3.0f;
-	public float spacing = 0.05f;
+	public List<float> radii;
+	public List<int> BuidlingsPerRing;
 
-
+	public float heightMultiplier = 2.0f;
+	public float heightVariance = 1.0f;
 
 
 	private Vector3 spawnPosition;
 	private GameObject building;
 	private GameObject centerBuilding;
 	private Vector3 lookPoint;
+	private Vector3 workingPosition;
 	// Use this for initialization
 	void Start () 
 	{
-
+		// Center Building is special, last building, spawn separately. 
 		centerBuilding = Instantiate(buildingPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+		centerBuilding.transform.SetParent(transform);
 		lookPoint = centerBuilding.transform.position;
 
-		for (int i = 0; i < numberOfBuildings; i++)
+		for (int ring = 0; ring < radii.Count; ring++)
 		{
-			spawnPosition.x = i*radius*Mathf.Sin(i*spacing);
-			spawnPosition.z = i*radius*Mathf.Cos(i*spacing);
-			spawnPosition.y = -Vector3.Distance(centerBuilding.transform.position,spawnPosition) * heightVariance;
-			building = Instantiate(buildingPrefab, spawnPosition, Quaternion.identity) as GameObject;
-			building.transform.SetParent(transform);
-			
-
-
+			for (int j = 0; j < BuidlingsPerRing[ring]; j++)
+			{
+				workingPosition = new Vector3(radii[ring]*Mathf.Cos(Mathf.Deg2Rad*j*360.0f/(BuidlingsPerRing[ring])), 0.0f, radii[ring]*Mathf.Sin(Mathf.Deg2Rad*j*360.0f/(BuidlingsPerRing[ring])));
+				//Debug.Log(workingPosition);
+				building = Instantiate(buildingPrefab, workingPosition, Quaternion.identity) as GameObject;
+				building.transform.SetParent(transform);
+				building.transform.position += -Vector3.up*heightMultiplier*radii[ring] + Vector3.up*Random.Range(0,heightVariance);
+				//lookPoint = Vector3.zero;
+				//building.transform.LookAt(lookPoint);
+			}
 		}
+
 	}
 	
 	// Update is called once per frame
