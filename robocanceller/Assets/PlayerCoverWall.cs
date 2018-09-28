23fe7242879529f3e92b9a3f0c8e86498e5e353e
Lucas Rumney <lucasrumney94@gmail.com>
2018-09-27 20:04:40 -0700

    add player area scaled cover wall

---
+++ robocanceller/Assets/PlayerCoverWall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerCoverWall : MonoBehaviour {
+
+    private float playAreaSizeX;
+    private float playAreaSizeZ;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ScaleToPlayArea();
+	}
+
+	void ScaleToPlayArea()
+	{
+		var rect = new Valve.VR.HmdQuad_t();
+		SteamVR_PlayArea.GetBounds(SteamVR_PlayArea.Size.Calibrated, ref rect);
+		transform.localScale = new Vector3(Mathf.Abs(rect.vCorners0.v0 - rect.vCorners2.v0), transform.localScale.y, transform.localScale.z);
+	
+	}
+}
